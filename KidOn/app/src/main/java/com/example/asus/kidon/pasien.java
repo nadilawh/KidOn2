649366f02cbd4515763e79f97d490870b782a42c
nadilawh <nadilawh0@gmail.com>
2018-09-22 07:41:34 -0700

    first

---
+++ KidOn/app/src/main/java/com/example/asus/kidon/pasien.java
@@ -0,0 +1,13 @@
+package com.example.asus.kidon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pasien extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pasien);
+    }
+}
